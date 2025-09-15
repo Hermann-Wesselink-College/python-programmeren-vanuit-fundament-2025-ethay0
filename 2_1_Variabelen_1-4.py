d0dd4a5741285df295e646c145f11737dfb1ce64
ethay0 <37144@lln.hethwc.nl>
2025-09-15 09:33:24 +0000

    modified:   2_1_Variabelen_1-4.py

--- 2_1_Variabelen_1-4.py
+++ 2_1_Variabelen_1-4.py
@@ -1,1 +1,14 @@
 # opgaven 1 tot en met 4 van paragraaf 2.1 Variabelen
+
+# Opgave 1:
+verzendkosten = 5
+# Opgave 2:
+productcode = "A136i"
+# Opgave 3: 
+verzendkosten = 5
+productcode = "A136i"
+print(verzendkosten)
+print(productcode)
+# Opgave 4:
+prijs = 9.95
+print(prijs)
