1816a0d7bbe8f0d1da65226e7b19ab11bfd669d8
ethay0 <37144@lln.hethwc.nl>
2025-09-18 08:27:42 +0000

    modified:   2_6_Variabelen_1-5.py

--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,21 @@
 # opdracht 1 tpt en met 5 van paragraaf 2.6
+# opdracht 1
+postcode = "1234AB"
+print(postcode.lower())
+# opdracht 2
+woonplaats = "Amstelveen"
+print(woonplaats.upper())
+# opdracht 3
+verzendmethode = "PostNL"
+betaalmethode = "iDEAL"
+print(verzendmethode.lower())
+print(betaalmethode.upper())
+# opdracht 4
+postcode = input("Wat is je postcode?")
+print(postcode.upper())
+# opdracht 5
+woonplaats = input("Wat is je woonplaats?")
+if len(woonplaats) == 1:
+    print("Jou woonplaats heeft 1 teken")
+else:
+    print("Jou woonplaats heeft " + str(len(woonplaats)) + " tekens")
--- 2_7_Variabelen_1-2.py
+++ 2_7_Variabelen_1-2.py
@@ -1,1 +1,14 @@
 # opdracht 1 en 2 van paragraaf 2.7
+# opdracht 1
+voornaam = input("Wat is je voornaam?")
+achternaam = input("Wat is je achternaam?")
+print("Hallo " + voornaam + " " + achternaam + "!")
+# opdracht 2
+naam = input("Wat is je naam?")
+straat = input("In welke straat woon je?")
+huisnummer = input("Wat is je huisnummer?")
+postcode = input("Wat is je postcode?")
+woonplaats = input("Wat is je woonplaats?")
+print(naam)
+print(straat + " " + huisnummer)
+print(postcode.upper() + "  " + woonplaats.upper())
