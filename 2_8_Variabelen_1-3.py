0413ffe0bebe0afe0dc0ac5e4e2566ad555cb7ca
ethay0 <37144@lln.hethwc.nl>
2025-09-22 08:56:36 +0000

    modified:   2_8_Variabelen_1-3.py 	modified:   2_9_Variabelen_1-6.py

--- 2_8_Variabelen_1-3.py
+++ 2_8_Variabelen_1-3.py
@@ -1,1 +1,20 @@
 # opdrachten 1 tot en met 3 van pragraaf 2.8
+# opdracht 1
+prijs = 24.95
+print("prijs: " + str(prijs))
+# opdracht 2
+voornaam = "Henk"
+achternaam = "de Vries"
+naam = voornaam + " " + achternaam
+naamLengte = len(naam)
+print("Mijn naam is " + naam)
+print("Mijn naam bestaat uit " + str(naamLengte) + " letters")
+print(voornaam.lower())
+print(achternaam.upper())
+# opdracht 3
+voornaam = input("Wat is je voornaam? ")
+achternaam = input("Wat is je achternaam? ")
+naam = voornaam + " " + achternaam
+naamLengte = len(naam)
+print("Je naam is: " + naam)
+print("Je naam bestaat uit " + str(naamLengte) + " Letters (Inclusief spaties)")
--- 2_9_Variabelen_1-6.py
+++ 2_9_Variabelen_1-6.py
@@ -1,1 +1,35 @@
 # opdrachten 1 tot en met 6 van paragraaf 2.9
+# Vraag 1
+totaalprijs = 50.00
+kortingsBon = 10.00
+nieuwePrijs = totaalprijs - kortingsBon
+print("Nieuwe prijs na korting: " + str(nieuwePrijs) + " EUR.")
+
+# Vraag 2
+cijfer1 = 8.5
+cijfer2 = 7.0
+gemiddelde = (cijfer1 + cijfer2) / 2
+print("Het gemiddelde van de cijfers is: " + str(gemiddelde))
+
+# Vraag 3
+prijsExclBtw = float(input("Voer het bedrag exclusief btw in: "))
+prijsInclBtw = prijsExclBtw * 1.21
+print("Bedrag inclusief 21% btw: " + str(prijsInclBtw) + " EUR.")
+
+# Vraag 4
+getal1 = int(input("Geef een getal: "))
+getal2 = int(input("Geef nog een getal: "))
+uitkomst = getal1 + getal2
+print("De optelling van deze getallen is: " + str(uitkomst))
+
+# Vraag 5
+bedrag = float(input("Voer het bedrag in euro's in: "))
+wisselkoers = 1.1648
+bedragDollar = bedrag * wisselkoers
+print("Het bedrag in Amerikaanse dollars is: " + str(bedragDollar))
+
+# Vraag 6
+totaalPrijs = float(input("Voer de totaalprijs in: "))
+kortingsPercentage = float(input("Voer het kortingspercentage in: "))
+totaalPrijsInclKorting = totaalPrijs * (1 - kortingsPercentage / 100)
+print("Totaalprijs incl. korting: " + str(totaalPrijsInclKorting) + " EUR.")
