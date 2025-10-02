80b52488de5e8969cc1408451f9c29964298db78
ethay0 <37144@lln.hethwc.nl>
2025-10-02 06:04:23 +0000

    modified:   2_10_Variabelen_1-7.py 	modified:   3_4_Selectie_1-9.py 	modified:   4_3_Iteratie_3-7.py

--- 2_10_Variabelen_1-7.py
+++ 2_10_Variabelen_1-7.py
@@ -1,1 +1,62 @@
 # Opdracht 1 tot en met 7 van pragraaf 2.10
+# Opdracht 1
+from datetime import datetime
+datum = datetime.now()
+maand = datum.month
+print(str(maand))
+# Opdracht 2
+from datetime import datetime
+datum = datetime.now()
+dag = datum.day
+print(str(dag))
+# Opdracht 3
+from datetime import datetime
+datum = datetime.now()
+dag = datum.day
+maand = datum.month
+jaar = datum.year
+print(str(dag) + "-" + str(maand) + "-" + str(jaar))
+# Opdracht 4
+from datetime import datetime
+datumtijd = datetime.now()
+minuut = datumtijd.minute
+print(str(minuut))
+# Opdracht 5
+from datetime import datetime
+datumtijd = datetime.now()
+minuut = datumtijd.minute
+uur = datumtijd.hour
+print(str(uur) + ":" + str(minuut))
+# Opdracht 6
+	from datetime import datetime
+datumtijd = datetime.now()
+minuut = datumtijd.minute
+uur = datumtijd.hour
+dag = datumtijd.day
+maand = datumtijd.month
+jaar = datumtijd.year
+ 
+datum = str(dag) + "-" + str(maand) + "-" + str(jaar)
+tijd = str(uur) + ":" + str(minuut)
+ 
+print("De datum van vandaag is " + datum + ". Het is nu " + tijd + ".")
+# Opdracht 7
+# a
+from datetime import datetime
+d0 = datetime(2017, 9, 11)
+d1 = datetime(1988, 3, 19)
+verschil = d0 - d1
+print(verschil.days)
+# b
+from datetime import datetime
+d0 = datetime.now()
+d1 = datetime(1988, 3, 17)
+verschil = d0 - d1
+print(verschil.days)
+# c
+from datetime import datetime
+d0 = datetime.now()
+d1 = datetime(1988, 3, 17)
+datumVerschil = d0 - d1
+dagenVerschil = datumVerschil.days / 365.25
+print(dagenVerschil)
--- 3_4_Selectie_1-9.py
+++ 3_4_Selectie_1-9.py
@@ -1,1 +1,28 @@
 # Opdrachten 1 tot en met 9 van paragraaf 3.4
+# Opdracht 1
+bedrag = 100
+if bedrag > 150:
+  print(bedrag * 1.19)
+else:
+  print(bedrag * 1.16)
+
+# Opdracht 2
+bedrag = 10
+if bedrag > 150:
+  print(bedrag * 1.19)
+elif bedrag < 55:
+  print(bedrag * 1.11)
+else:
+  print(bedrag * 1.16)
+  
+# Opdracht 3
+bedrag = float(input("Voer een bedrag in"))
+if bedrag > 150:
+  nieuwBedrag = bedrag * 1.19
+  print(nieuwBedrag)
+elif bedrag < 55:
+  nieuwBedrag = bedrag * 1.11
+  print(nieuwBedrag)
+else:
+  nieuwBedrag = bedrag * 1.16
+  print(nieuwBedrag)
